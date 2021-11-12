03782ce377221acbbd3dc93e3e755558dbdf2774
Jhower Orozco <jhower.saldarriaga@integralsoftware.com.co>
2021-11-12 17:10:12 -0500

    arreglo marcador de quienes van  ingresando

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,27 @@
-var map = L.map('map-template').setView([51.505, -0.09], 3);
-
+const map = L.map('map-template').setView([4.610, -74.082], 13);
 
 const tile = L.tileLayer('https://b.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
 // Socket Io
 const socket = io.connect();
 
-// Marker
-const marker = L.marker([50.5, 30.5]); // kiev, ukraine
-marker.bindPopup('Hello There!');
-map.addLayer(marker);
-
 // Geolocation
-map.locate({enableHighAccuracy: true})
-map.on('locationfound', (e) => {
-    const coords = [e.latlng.lat, e.latlng.lng];
+map.locate({enableHighAccuracy: true});
+
+map.on('locationfound', event => {
+    const coords = [event.latlng.lat, event.latlng.lng];
     const newMarker = L.marker(coords);
     newMarker.bindPopup('You are Here!');
     map.addLayer(newMarker);
-    socket.emit('userCoordinates', e.latlng);
+    socket.emit('userCoordinates', event.latlng);
 });
 
 // socket new User connected
-socket.on('newUserCoordinates', (coords) => {
-    console.log(coords);
-    const userIcon = L.icon({
-        iconUrl: '/img/icon2.png',
-        iconSize: [38, 42],
-    })
-    const newUserMarker = L.marker([coords.lat, coords.lng], {
-        icon: userIcon
-    });
+socket.on('newUserCoordinates', coords => {
+    const newUserMarker = L.marker([coords.lat, coords.lng], {icon: userIcon});
     newUserMarker.bindPopup('New User!');
     map.addLayer(newUserMarker);
+    map.setView([4.610, -74.082]);
 });
 
 map.addLayer(tile);
